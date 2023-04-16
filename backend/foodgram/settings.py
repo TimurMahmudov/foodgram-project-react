dc2cf31022d87d7518bd2d50c57eb4e75d94c7aa
TimurMahmudov <verloren454@gmail.com>
2023-04-16 22:21:48 +0300

    Последние правки

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'PAGE_SIZE': 'limit',
 }
 
 DJOSER = {
